1ddf9b69b180683ee23ff236ac8d76f32b6af5de
Sujeet Srivastava <55811102+Sujeet-s@users.noreply.github.com>
2020-10-18 17:20:57 +0530

    Update textfromvoice.py

--- textfromvoice.py
+++ textfromvoice.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
+import docx as do
 
+doc=do.Document()
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
@@ -8,6 +10,8 @@ with sr.Microphone() as source:
     print('Done')
     try:
         text = r.recognize_google(audio)
+        doc.add_paragraph(text)
+        doc.save('finally.docx')
         print("You said : {}".format(text))
     except:
         print("Sorry could not recognize what you said")
